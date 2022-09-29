54cc436fcc2869e52c04aeb42287d5ec5a7a7efb
prashantbharate <prshntbharte77@gmail.com>
2022-09-29 09:59:19 +0530

    commented

--- src/main/java/com/masai/controller/LoginController.java
+++ src/main/java/com/masai/controller/LoginController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+//sangram 
 import com.masai.exception.LogInException;
 import com.masai.model.LogInData;
 import com.masai.service.LogInService;
